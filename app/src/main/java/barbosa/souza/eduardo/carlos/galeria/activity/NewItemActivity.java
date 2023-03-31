65e9e28dc5e0d9c07d5e65611219e83d340a2005
20211TIIMI0303 <cesbarbosa2005@gmail.com>
2023-03-31 09:05:57 -0300

    lista atividade3

---
+++ app/src/main/java/barbosa/souza/eduardo/carlos/galeria/activity/NewItemActivity.java
@@ -0,0 +1,15 @@
+package barbosa.souza.eduardo.carlos.galeria.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import barbosa.souza.eduardo.carlos.galeria.R;
+
+public class NewItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+}
